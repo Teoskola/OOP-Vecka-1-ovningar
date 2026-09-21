508762db77b36b4bc888a6c31a4ab12418210a66
Teodor <Teodor@TEODOR>
2026-09-21 11:14:40 +0200

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Vecka_1_Övningar
+{
+    internal class BankAccount
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Vecka_1_Övningar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
