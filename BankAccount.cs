87c71a143ebd7bfd69c9d5586f51b65908042e9d
Teodor <Teodor@TEODOR>
2026-09-21 11:57:32 +0200

    Skapar privat balance på BankAccount.cs och deposit samt withdraw metoder som uppdaterar balance med objekt i program.cs.

--- BankAccount.cs
+++ BankAccount.cs
@@ -6,6 +6,35 @@ namespace OOP_Vecka_1_Övningar
 {
     internal class BankAccount
     {
+        //Attributer 
+        private decimal balance;
+
+        //konstruktör
+        public BankAccount(decimal initialBalance) //konstruktör som tar ett initialt saldo som parameter
+        {
+            balance = initialBalance;
+            Console.WriteLine($"Your account has been created with an initial balance of {initialBalance}.");
+        }
+
+        //Metoder
+        public void Deposit(decimal amount) //metod för att sätta in pengar på kontot
+        {
+            Console.WriteLine($"{amount} has been deposited to your account. your new balance is {balance + amount}.");
+                  balance += amount; // Uppdaterar saldot efter insättning    
+        }
+
+        public void Withdraw(decimal amount) //metod för att ta ut pengar från kontot
+        {
+            Console.WriteLine($"{amount} has been withdrawn from your account. your new balance is {balance - amount}.");
+            if (amount > balance) //kontrollerar om det finns tillräckligt med pengar på kontot
+            {
+                Console.WriteLine("Insufficient funds for this withdrawal.");
+            }
+            else
+            {
+                balance -= amount; // Uppdaterar saldot efter uttag
+            }
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            //Skapar ett objekt av klassen BankAccount och använder dess metoder
+            BankAccount account1 = new BankAccount(100); // Skapar ett nytt bankkonto med ett initialt saldo på 100
             
+            account1.Deposit(500); // Sätter in 500 på kontot
+            account1.Withdraw(200); // Tar ut 200 från kontot
+
         }
     }
 }
